36fd5e735243671b364bd742d679a45043ca4c28
will3g <willianpaulo_09@hotmail.com>
2020-05-10 15:50:09 -0300

    create-toggle-menu

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -5,17 +5,33 @@ export default class menu extends Component {
     render() {
         return (
             <>
-                <div class="page">
-                    <nav class="menu">
+                <nav class="page">
+                    <div class="menuDesk">
                         <ul class="menu__list">
-                            <li class="menu__group"><a href="#0" class="menu__link">Lorem #1</a></li>
-                            <li class="menu__group"><a href="#0" class="menu__link">Lorem #2</a></li>
-                            <li class="menu__group"><a href="#0" class="menu__link">Lorem #3</a></li>
-                            <li class="menu__group"><a href="#0" class="menu__link">Lorem #3</a></li>
-                            <li class="menu__group"><a href="#0" class="menu__link">Lorem #3</a></li>
+                            <li class="menu__group"><a href="#0" class="menu__link">Lorem</a></li>
+                            <li class="menu__group"><a href="#0" class="menu__link">Lorem</a></li>
+                            <li class="menu__group"><a href="#0" class="menu__link">Lorem</a></li>
+                            <li class="menu__group"><a href="#0" class="menu__link">Lorem</a></li>
+                            <li class="menu__group"><a href="#0" class="menu__link">Lorem</a></li>
                         </ul>
-                    </nav>
-                </div>
+                    </div>
+                    <div id="menuToggle">
+                        <input type="checkbox" />
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <ul id="menu">
+                            <li><a href="#">Lorem</a></li>
+                            <li><a href="#">Lorem</a></li>
+                            <li><a href="#">Lorem</a></li>
+                            <li><a href="#">Lorem</a></li>
+                            <li><a href="#">Lorem</a></li>
+                            <li><a href="#">Lorem</a></li>
+                            <li><a href="#">Lorem</a></li>
+                            <li><a href="#">Lorem</a></li>
+                        </ul>
+                    </div>
+                </nav>
             </>
         );
     }
