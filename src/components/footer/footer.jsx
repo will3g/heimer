3149c5fa8367871349d0cec6b6dcaa775e3dcf43
will3g <willianpaulo_09@hotmail.com>
2020-05-24 21:04:31 -0300

    creating-component-to-footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -10,18 +10,40 @@ export default class Footer extends Component {
     }
 
     componentDidMount() {
-        this.setState({ logo : this.props.logo })
-    }  
+        this.setState({ logo: this.props.logo })
+    }
 
     render() {
-        return(
-            <footer>
-                <ul>
-                    <li>Home</li>
-                    <li>Materias</li>
-                    <li>Contato</li>
-                    <li>Sobre</li>
-                </ul>
+        return (
+            <footer className="footer">
+                <div class="col-12 col-xl-8">
+                    <ul class="footer-nav">
+                        <li class="footer-nav-item">
+                            <a href="#" class="footer-nav-link" target="_blank">Lorem</a>
+                        </li>
+                        <li class="footer-nav-item">
+                            <a href="#" class="footer-nav-link" target="_blank">Lorem</a>
+                        </li>
+                        <li class="footer-nav-item">
+                            <a href="#" class="footer-nav-link" target="_blank">Lorem</a>
+                        </li>
+                        <li class="footer-nav-item">
+                            <a href="#" class="footer-nav-link" target="_blank">Lorem</a>
+                        </li>
+                        <li class="footer-nav-item">
+                            <a href="#" class="footer-nav-link" target="_blank">Lorem</a>
+                        </li>
+                        <li class="footer-nav-item">
+                            <a href="#" class="footer-nav-link" target="_blank">Lorem</a>
+                        </li>
+                        <li class="footer-nav-item">
+                            <a href="#" class="footer-nav-link" target="_blank">Lorem</a>
+                        </li>
+                    </ul>
+                    <div class="footer-copy">
+                        <p className="copy">© Copyright 2020 - LOREM, todos os direitos reservados.</p>
+                    </div>
+                </div>
             </footer>
         );
     }
