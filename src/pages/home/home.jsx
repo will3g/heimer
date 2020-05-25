85de9fd76dd081c0ac4ee8ea4ba5df58bdea8d68
will3g <willianpaulo_09@hotmail.com>
2020-05-24 21:01:44 -0300

    styling-home-in-desktop

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,39 +3,50 @@ import React from 'react';
 import api from "../../services/home.json";
 
 import Menu from '../../components/menu/menu';
-import FeaturedMain from '../../components/featured/featuredMain';
 import Featured from '../../components/featured/featured';
+import FeaturedMain from '../../components/featured/featuredMain';
+import Advertising from '../../components/advertising/advertising';
+import Footer from '../../components/footer/footer';
 
 import '../../assets/styles/css/components/menu/menu.css';
+import '../../assets/styles/css/components/advertising/advertising.css';
 import '../../assets/styles/css/components/featured/featured.css';
+import '../../assets/styles/css/components/footer/footer.css';
+import '../../assets/styles/css/components/featured/featured-main.css';
 
-export default function article() {
+export default function Home() {
 
     return(
         <>
             <Menu/>
-            {api.map(e => {
-                    if (e.featured_main) {
-                        return( 
-                            <FeaturedMain 
-                                images={e.images} 
-                                title={e.title} 
-                                description={e.description} 
-                                typeContent={e.type_content}
-                            />
-                        )
-                    } else {
-                        return(
-                            <Featured 
-                                images={e.images} 
-                                title={e.title} 
-                                description={e.description} 
-                                typeContent={e.type_content}
-                            />
-                        )
+            <div className="content">
+                {api.map((e, index) => {
+                        if (e.featured_main) {
+                            return( 
+                                <FeaturedMain 
+                                    images={e.images} 
+                                    title={e.title} 
+                                    description={e.description} 
+                                    typeContent={e.type_content}
+                                />
+                            )
+                        } else {
+                            return(
+                                <Featured 
+                                    item={index}
+                                    images={e.images} 
+                                    title={e.title} 
+                                    description={e.description} 
+                                    typeContent={e.type_content}
+                                />
+                            )
+                        }
                     }
-                }
-            )}
+                )}
+                <Advertising adv="fake" item={1}/>
+                <Advertising adv="fake" item={2}/>
+            </div>
+            <Footer/>
         </>
     );
 }
