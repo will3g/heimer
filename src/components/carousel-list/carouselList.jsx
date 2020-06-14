a6586f3ec96565d48b620bf8fc435b29770b8cdd
will3g <willianpaulo_09@hotmail.com>
2020-06-14 14:41:51 -0300

    fix-carrousel-list

---
+++ src/components/carousel-list/carouselList.jsx
@@ -0,0 +1,69 @@
+// Componentes: Thumbnail, Título, Type, description
+import React, { Component } from 'react';
+
+export default class carouselList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            api: ''
+        };
+    }
+
+    componentWillMount() {
+        let api = this.props.api;
+        this.setState({ api });
+    }
+
+    render() {
+
+        const attr = {
+            image: {
+                width: '95px',
+                height: '75px'
+            }
+        }
+
+        return (
+            <amp-list layout="fixed-height" src={this.state.api}>
+                {
+                    this.state.api.map((e, index) => {
+                        if (index == 0) {
+                            return (
+                                <div option={index} selected key={index} className="artile-list">
+                                    <a href={e.url}>
+                                        <amp-img className="article-image selected" src={e.images} 
+                                        width={attr.image.width} height={attr.image.height}></amp-img>
+                                        <span className="article-title">{e.title}</span>
+                                    </a>
+                                </div>
+                            )
+                        }
+
+                        if (index == 4) {
+                            return (
+                                <div option={index} id="id5" key={index} className="artile-list">
+                                    <a href={e.url}>
+                                        <amp-img className="article-image selected" src={e.images} 
+                                        width={attr.image.width} height={attr.image.height}></amp-img>
+                                        <span className="article-title">{e.title}</span>
+                                    </a>
+                                </div>
+                            )
+                        }
+
+                        return (
+                            <div option={index} key={index} className="artile-list">
+                                <a href={e.url}>
+                                    <amp-img className="article-image" src={e.images} 
+                                    width={attr.image.width} height={attr.image.height}></amp-img>
+                                    <span className="article-title">{e.title}</span>
+                                </a>
+                            </div>
+                        )
+                    })
+                }
+            </amp-list>
+        );
+    }
+}
