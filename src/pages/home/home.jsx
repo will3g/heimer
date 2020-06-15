5509a3c1744b3d6447f324c25ba1d17b8acde4fe
will3g <willianpaulo_09@hotmail.com>
2020-06-15 18:50:45 -0300

    test-components

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 
+import api from "../../services/home.json";
 import Menu from '../../components/menu/menu';
+import Carousel from '../../components/carousel/carousel';
+import CarouselList from '../../components/carousel-list/carouselList';
 import Footer from '../../components/footer/footer';
 
 import '../../assets/styles/css/components/main.min.css';
+import '../../assets/styles/css/components/carousel.css';
+import '../../assets/styles/css/components/carrouselList.css';
+import '../../assets/styles/css/components/image-carousel.css';
 
 export default function Home() {
 
-    return(
+    return (
         <>
-            <Menu/>
-            <Footer/>
+            {/* <Menu/> */}
+            <Carousel api={api}/>
+            {/* <CarouselList api={api}/> */}
+            {/* <Footer/> */}
         </>
     );
 }
