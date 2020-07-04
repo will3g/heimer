87e52582e98c2da7c34fd38bd7c8574d1c7f62ba
will3g <willianpaulo_09@hotmail.com>
2020-07-04 16:45:05 -0300

    test-newslist

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
 
+//import api from "../../services/review.json";
 import api from "../../services/home.json";
-import Menu from '../../components/menu/menu';
-import Carousel from '../../components/carousel/carousel';
-import CarouselList from '../../components/carousel-list/carouselList';
-import Footer from '../../components/footer/footer';
 
-import '../../assets/styles/css/components/main.min.css';
-import '../../assets/styles/css/components/carousel.css';
-import '../../assets/styles/css/components/carrouselList.css';
-import '../../assets/styles/css/components/image-carousel.css';
+// import Menu from '../../components/menu/menu';
+// import Carousel from '../../components/carousel/carousel';
+// import CarouselList from '../../components/carousel-list/carouselList';
+// import Review from '../../components/review/review';
+// import Footer from '../../components/footer/footer';
+import Newslist from '../../components/newslist/newslist';
+
+//import '../../assets/styles/css/components/main.min.css';
+
+import '../../assets/styles/css/components/newslist.css';
 
 export default function Home() {
 
     return (
         <>
-            {/* <Menu/> */}
-            <Carousel api={api}/>
-            {/* <CarouselList api={api}/> */}
-            {/* <Footer/> */}
+            <Newslist api={api}/>
         </>
     );
 }
+
+
+// {/* <Menu/> */}
+// {/* <Carousel api={api}/> import '../../assets/styles/css/components/carousel.css'; import '../../assets/styles/css/components/image-carousel.css';*/}
+// {/* <Review api={api}/>  import '../../assets/styles/css/components/review.css'; */}
+// {/* <Footer/> */}
