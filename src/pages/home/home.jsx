19b2a8a7d5f60232fd847d96daa261fa93602557
will3g <willianpaulo_09@hotmail.com>
2020-07-18 15:01:27 -0300

    add-newslist-to-subhome

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 // UTILIZADO EM NEWSLIST E CAROUSEL
 //import api from "../../services/home.json";
 
+// UTILIZADO EM NEWSLIST DE SUBHOME
+import api from "../../services/subhome-newslist.json";
+
 // UTILIZADO EM REVIEW
 //import api from "../../services/review.json";
 
 // UTILIZADO EM CONSOLES
-import api from "../../services/consoles.json";
+//import api from "../../services/consoles.json";
 
 // UTILIZADO EM MAIS LIDAS
 //import api from "../../services/most-read.json";
@@ -16,15 +19,17 @@ import api from "../../services/consoles.json";
 //import Menu from '../../components/menu/menu';
 //import Footer from '../../components/footer/footer';
 //import Review from '../../components/review/review';
-import Consoles from '../../components/consoles/consoles';
+//import Consoles from '../../components/consoles/consoles';
 //import Newslist from '../../components/newslist/newslist';
+import NewslistSubhome from '../../components/newslist/newslist-subhome';
 //import Mostread from '../../components/most-read/mostRead';
 
 // ESTILIZAÇÃO COMPONENTES
 //import '../../assets/styles/css/components/review.css';
-import '../../assets/styles/css/components/consoles.css';
+//import '../../assets/styles/css/components/consoles.css';
 //import '../../assets/styles/css/components/main.min.css';
 //import '../../assets/styles/css/components/newslist.css';
+import '../../assets/styles/css/components/newslist-subhome.css';
 //import '../../assets/styles/css/components/most-read.css';
 
 // UTILIZADO EM CARROSSEL
@@ -48,9 +53,9 @@ export default function Home() {
             {/* <Menu/> */}
             {/* <Mostread api={api}/> */}
             {/* <Carousel api={api}/> */}
-            {/* <Newslist api={api}/> */}
+            <NewslistSubhome api={api}/>
             {/* <Review api={api}/> */}
-            <Consoles api={api}/>
+            {/* <Consoles api={api}/> */}
             {/* <Footer/> */}
         </>
     );
