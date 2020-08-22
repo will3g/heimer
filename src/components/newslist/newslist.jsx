c97e7a3c6e3938a3527de97cfc4faf0f1ae7f8da
will3g <willianpaulo_09@hotmail.com>
2020-08-22 14:55:40 -0300

    fix-component-and-passing-css-to-scss

--- src/components/newslist/newslist.jsx
+++ src/components/newslist/newslist.jsx
@@ -31,40 +31,40 @@ export default class newslist extends Component {
         }
 
         return (
-            <amp-list id="newslist-container" layout="fixed-height" src={this.state.api}>
+            <amp-list id="newslist-container-1" layout="fixed-height" src={this.state.api}>
                 <div>
-                    <header class="home-news-list-header">
-                        <a href="/novidades/" class="home-news-list-header-title">Novidades</a>
-                        <span class="separator"></span>
-                        <a href="/novidades/" class="home-news-list-header-tags-btn">ver todas</a>
+                    <header class="newslist-header">
+                        <a href="/novidades/" class="newslist-header-title">Novidades</a>
+                        <span class="separator-newslist"></span>
+                        <a href="/novidades/" class="newslist-header-tags-btn">ver todas</a>
                     </header>
                     {
                         this.state.api.map((e, index) => {
                             if (index % 4) {
                                 return (
-                                    <div option={index} key={index} className="article-normal info">
+                                    <div option={index} key={index} className="article-normal-newslist info-newslist">
                                         <a href={e.url}>
-                                            <amp-img className="article-image" src={e.images} 
+                                            <amp-img src={e.images} 
                                             width={attr.article.normal.width} height={attr.article.normal.height}></amp-img>
                                         </a>
                                         <div>
-                                            <span className="home-vertical-news-info link">há mais de 16 horas</span>
-                                            <a className="link home-vertical-news-title" href={e.url}><span>{e.title}</span></a>
-                                            <a className="home-vertical-news-info link" href='/plataformas'><span>Plataformas</span></a>
+                                            <span className="vertical-newslist-info link-newslist">há mais de 16 horas</span>
+                                            <a className="link-newslist vertical-newslist-title" href={e.url}><span>{e.title}</span></a>
+                                            <a className="vertical-newslist-info link-newslist" href='/plataformas'><span>Plataformas</span></a>
                                         </div>
                                     </div>
                                 )
                             }
 
                             return (
-                                <div option={index} key={index} className="article-main info">
+                                <div option={index} key={index} className="article-main-newslist info-newslist">
                                     <a href={e.url}>
-                                        <amp-img className="article-image" src={e.images} 
+                                        <amp-img src={e.images} 
                                         width={attr.article.main.width} height={attr.article.main.height}>
                                             <div>
-                                                <span className="home-big-more-info">há mais de 16 horas</span>
-                                                <a className="home-big-news-title" href={e.url}><span>{e.title}</span></a>
-                                                <a className="home-big-more-info" href='/plataformas'><span>Plataformas</span></a>
+                                                <span className="home-big-more-info-newslist">há mais de 16 horas</span>
+                                                <a className="home-big-title-newslist" href={e.url}><span>{e.title}</span></a>
+                                                <a className="home-big-more-info-newslist" href='/plataformas'><span>Plataformas</span></a>
                                             </div>
                                         </amp-img>
                                     </a>
