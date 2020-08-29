ad33cac58979bbc0a287209defe1d66fe35835d1
will3g <willianpaulo_09@hotmail.com>
2020-08-29 13:00:15 -0300

    build

---
+++ build/precache-manifest.96562a4d3001ae14144b080e749aea58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1f24f26eceabb254bd7e548cfa7e8ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a048d784876d7fbe0407",
+    "url": "/static/css/main.14396513.chunk.css"
+  },
+  {
+    "revision": "feb5a7b6bc989aff03f6",
+    "url": "/static/js/2.118c5e22.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.118c5e22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a048d784876d7fbe0407",
+    "url": "/static/js/main.59d91228.chunk.js"
+  },
+  {
+    "revision": "228435cee4c7a0c3bfd5",
+    "url": "/static/js/runtime-main.68e7860e.js"
+  }
+]);
