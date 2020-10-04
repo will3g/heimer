b88ffcb394617f373cde6a0f250c332a2f7e331c
will3g <willianpaulo_09@hotmail.com>
2020-10-04 15:34:39 -0300

    fix-article-page

--- src/pages/article/article.jsx
+++ src/pages/article/article.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import articleAPI from "../../services/article.json";
 
@@ -14,20 +14,61 @@ import TypeContent from "../../components/type/typeContent";
 import PublicationDate from "../../components/publication/publicationDate";
 import Thumbnail from "../../components/thumbnail/thumbnail";
 
-export default function article() {
-    return(
-        <>
-            <Title title={articleAPI.title}/>
-            <Thumbnail thumbnail={articleAPI.thumbnail}/>
-            <Description description={articleAPI.description}/>
-            <Author author={articleAPI.author}/>
-            <PublicationDate publicationDate={articleAPI.publication_date}/>
-            <Sponsor sponsor={articleAPI.sponsor}/>
-            <TypeContent typeContent={articleAPI.type_content}/>
-            <Images images={articleAPI.images} id="1"/>
-            <Videos videos={articleAPI.videos} id="1"/>
-            <Content content={articleAPI.content}/>
-            <Footer logo={articleAPI.footer.logo}/>
-        </>
-    );
+export default class Article extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            thumbnail: '',
+            description: '',
+            author: '',
+            publication_date: '',
+            sponsor: '',
+            type_content: '',
+            images: [],
+            videos: '',
+            content: '',
+            footer_logo: ''
+        };
+    }
+
+    componentWillMount() {
+        articleAPI.forEach(article => {
+            let url = `/${window.location.href.split('/')[3]}`;
+            if (url == article.url) {
+                this.setState({ 
+                    title: article.title,
+                    thumbnail: article.thumbnail,
+                    description: article.description,
+                    author: article.author,
+                    publication_date: article.publication_date,
+                    sponsor: article.sponsor,
+                    type_content: article.type_content,
+                    images: article.images,
+                    videos: article.videos,
+                    content: article.content,
+                    footer_logo: article.footer.logo
+                })
+            }
+        });
+    }
+
+    render() {
+        return(
+            <>
+                <Title title={this.state.title}/>
+                <Thumbnail thumbnail={this.state.thumbnail}/>
+                <Description description={this.state.description}/>
+                <Author author={this.state.author}/>
+                <PublicationDate publicationDate={this.state.publication_date}/>
+                <Sponsor sponsor={this.state.sponsor}/>
+                <TypeContent typeContent={this.state.type_content}/>
+                <Images images={this.state.images} id="1"/>
+                <Videos videos={this.state.videos} id="1"/>
+                <Content content={this.state.content}/>
+                <Footer logo={this.state.footer_logo}/>
+            </>
+        );
+    }
 }
